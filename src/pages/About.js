6763134e6306a963d1d58dc68cbb4847f99ee605
Natalie Tang <natalietang77@gmail.com>
2023-05-03 15:29:18 -0500

    completed about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,35 @@
+import "./About.css";
+import aboutPic from "../components/images/about.png";
+
 export default function AboutPage() {
-    return (
-        <>
-        <h2>This is the about page!</h2>
-        </>
-    )
-};
+  return (
+    <>
+      <div className="App-header">
+        <div className="about-container">
+          <img src={aboutPic} alt="about pic" className="about-pic" />
+          <div className="text-container">
+            <div>hello! I'm Natalie, nice to meet you!</div>
+            <p>
+              for a while, I've struggled to find true happiness within myself
+              and often turned to journaling to help process my thoughts and
+              emotions. however, I found that writing affirmations towards
+              myself was a challenge, and didn't know what to write on the spot.
+              that's when the idea for love within was created! love within
+              provides these daily affirmations and journal prompts to help
+              guide you on your self-love journey.{" "}
+            </p>
+            <div>
+              my biggest inspiration was from “The Five Minute Journal,” where I
+              first journaled in and started my path to personal growth.{" "}
+            </div>
+            <div>remember, this is a safe space.</div>
+            <div className="inline-container">
+              <div>please be kind to yourself and always <span className="about-lovewithin">love within</span></div>
+            </div>
+            <div>have a question? <a href="/contact" className="ask-link">ask me!</a></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}

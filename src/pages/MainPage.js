809e7512fdeba119a7d4b033879d4891adf176d0
Natalie Tang <natalietang77@gmail.com>
2023-05-02 21:35:40 -0500

    completed main page

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,54 +1,57 @@
-import logo from "../logo.svg"
-import  { JournalEntryModel } from "../models/";
-import { DataStore } from "aws-amplify";
-
-// Create a function with this code:
-async function testEntry(){
-try {
-  const now = new Date();
-  const awsDate = now.toISOString().slice(0, 10);
-  const post = await DataStore.save(
-    new JournalEntryModel({
-      date: awsDate,
-      gratitude: "test",
-      dailyGoals: "test",
-      affirmation: "test",
-      amazingMoment: "test",
-      improvement: "test",
-      notes: "test"
-    })
-  );
-  console.log('Post saved successfully!', post);
-} catch (error) {
-  console.log('Error saving post', error);
-}
-}
-
-// Create a button to where if we click it, it executes the function
+import "./MainPage.css";
+import mainPageBanner from "../components/images/mainpage-banner.png";
+import selfLove from "../components/images/self-love-pic.png";
+import heartPlane from "../components/images/heart-plane.png";
+import heartPic from "../components/images/heart.png";
+import writePic from "../components/images/write.png";
 
 export default function MainPage() {
-    return (
-        <>
-            <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
+  return (
+    <div className="App-header">
+      <img src={mainPageBanner} alt="banner" className="banner" />
+      <div className="welcome-container">
+        <div>
+          <div className="call-to-action-container">
+            <h1 className="welcome">welcome to</h1>
+            <h1 className="lovewithin">love within</h1>
+          </div>
+          <div className="welcome-paragraph">
             <p>
-              Edit <code>src/App.js</code> and save to reload.
+              love within is your daily companion for cultivating self-love and
+              affirmations. through daily affirmations and inspiring messages,
+              love within empowers you to discover the love and beauty within
+              yourself. with a built-in journaling feature, you can capture your
+              thoughts and emotions in a safe and supportive space. let love
+              within guide you on a journey towards a happier, more fulfilled
+              you.
             </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-
-            </a>
-            <button onClick={testEntry}>
-                click
-              </button>
-          </header>
+          </div>
         </div>
-        </>
-    )
-};
+        <img src={selfLove} alt="self love pic" className="self-love-pic" />
+      </div>
+      <div className="heart-plane-container">
+        <img src={heartPlane} alt="heart plane" className="heart-plane-pic"/>
+      </div>
+      <div className="second-container">
+        <div className="column1">
+          <h1>receive a</h1>
+          <div class="button-container">
+            <a href="/dailyaffirmations">
+              <button className="second-container-button">daily affirmation</button>
+            </a>
+          </div>
+          <img src={heartPic} alt="heart pic" className="icons" />
+        </div>
+        <div className="column2">
+          <h1>begin</h1>
+          <div class="button-container">
+            <a href="/journal">
+              <button className="second-container-button">journaling</button>
+            </a>
+          </div>
+          <img src={writePic} alt="write pic" className="icons" />
+        </div>
+      </div>
+    </div>
+  );
+}
